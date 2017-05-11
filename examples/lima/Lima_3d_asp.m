45476321e7414cd3f7862902273031003404fef4
Percy Galvez <percy.galvez.barron@gmail.com>
2017-05-11 23:26:15 +0200

    update lima model, in progress

--- examples/lima/Lima_3d_asp.m
+++ examples/lima/Lima_3d_asp.m
@@ -14,18 +14,36 @@ rand(1,floor(sum(100*clock)));
 
 year = 3600*24*365; % Should we put a longer timeframe? Would this timeline
                     % allow us to see the big events (Mw>8.5)?(Luis)
+                    % PG: this is only a unit.
 p = qdyn('set');
 
+p.TMAX = 100000*year % Duration of the whole simulation.
+
+p.DYN_TH_ON = 0.1; % Slip velocity at the nucletion phase starts.
+p.DYN_TH_OFF = 0.001;
+
+co = 4e6; % Cohesion imposed in the first kilometers depth.
+co_limit = 2e3;
+p.MU = 32.5e9; % Shear Modulus.
+p.LAM = p.MU
+p.VS = 3000.0
+p.MU_SS = 0.6; % Reference friction coefficient.
+
 p.MESHDIM=2;   % I think we can preserve the mesh size for now (Luis).
-p.THETA_LAW=2; % 1) Should we change this? Would this be reasonable for the 
+p.THETA_LAW=1; % 1) Should we change this? Would this be reasonable for the 
                % Peru subduction zone? (Luis)
-p.SIGMA=0.5e6; % 1) Should we change this? Would this be reasonable for the 
+               % PG: Please USE p.THETA_LAW = 1, Ageing LAW.
+p.SIGMA=100.0e6; % 1) Should we change this? Would this be reasonable for the 
                % Peru subduction zone? 
                % 2) In the manual, it says that the normal stress remains 
                % constant unless it equals 1. Do we want this behavior in 
                % our model? (Luis)
+               % PG: The normal stress (P.SIGMA) should change with depth and 
+               %  constant value. Here 100 Mpa, but this could be changed.
 %p.V_SS=1e-9;
-p.V_SS=1.97e-9; % This is the relative velocity (m/s) of Nazca and South 
+p.V_SS=0.020/year % 20mm/year %1.97e-9;
+                % PG : Using Villegas et al. (2016) GPS survey, Figure 3.
+                % This is the relative velocity (m/s) of Nazca and South 
                 % America plates extracted from Kendrick et al (2003) Azimut 82NE.
                 % Is this OK for the steady-state slip  velocity? or do we
                 % have to consider creeping of the locking slip?  (Luis)
@@ -33,18 +51,24 @@ p.A=0.003; % 1) Should we change this? Would this be reasonable for the
            % Peru subduction zone? How does this relate to
            % different slip coupling in different locations in
            % the fault? (Luis)
+           % PG: P.A and P.B should change with depth.
 p.B=0.01;  % Same question than for p.A (Luis)
 p.SIGMA_CPL=1; % 1) Is this related to p.SIGMA? Do you agree if we start the 
                % modeling with a coupled normal stress (p.SIGMA_CPL=1)? (Luis)
+               % PG: This is relating with coupling of the normal stress with seismic waves.
+%		SIGMA_CPL  normal stress coupling (only for dipping faults)
+%			0 = disable
+%			1 = enable
 p.V2=0.01; % 1) Should we change this? Would this be reasonable for the 
-               % Peru subduction zone? (Luis)
+           % Peru subduction zone? (Luis)
 %p.L=80e3;
 p.L=800e3; % Along-strike length of the faults (in meters) extracted from 
            % the fault geometry 
            % Is it the lenght of the fault? It is not clear from the manual 
            % for MESHDIM=2 (Luis)
+           % PG : Yes. 
 %p.W=80e3;
-p.W=200e3; % Along-slip horizontal length of the fault (in meters) extracted 
+WH= 200e3; % Along-slip horizontal length of the fault (in meters) extracted 
            % from the fault geometry. This is the lenght of only the 
            % superficial part of the fault. 
            % 1) In the manual, it says that this parameter is distance between
@@ -56,32 +80,41 @@ p.W=200e3; % Along-slip horizontal length of the fault (in meters) extracted
            % the initial 300 km of horizontal length and reach 110 km of 
            % depth, but the dip angle changes a bit after the 80 of depth. 
            % Would the model be able to incorporate this?
+           % PG : I do not think we should go down to more than 80 km depth. 
+           % At this depth, the megathurst events do not nucleate anymore
+           % and normally ruptures do not go that deep.
            % 3) If we model below the 80 km of depth, we might have to
            % conseider that the properties of the fault are a bit different
            % below the 80 km, since the earthquake rates and magnitudes are
            % not as big as in the superficial part of the fault.
            % Any comments on this? (Luis)
-p.W=p.W/cos(17/180*pi); %Along dip distance
-p.NX=100;
-p.NW=30; % Related to question on p.W. Is this related to the contact 
+           % PG: At this depth the code imposes creeping zone.
+p.W=WH/cos(15/180*pi); %Along-dip distance, 
+           % Dip angle 15 degress according to Villegas et al.(2016). Table 1.
+p.NX=400;%100;
+p.NW=100;%30; % Related to question on p.W. Is this related to the contact 
          % surface? (Luis)
+         % PG : This is the number of elements along-dip fault. 
+         % This value is too small. To get stable running the grid size (p.W/p.NW)=2km.
 
-p.Z_CORNER=-60e3; % In Km (extracted from Nocquet, 20146). A report from 
+p.Z_CORNER=-WH/sind(15) %-60e3;
+                  % PG : Should be the end of the along-dip fault. 
+                  % In Km (extracted from Nocquet, 20146). A report from 
                   % Dr. Aguilar, mentions that active fault starts 30 km
                   % below the surface. Therefore in the superficial part of
                   % the fault we have: 80 km - 30 km = 50 km.
                   % Any comments on the fault starting at 30 km of depth?
                   % Should we start modeling it from depth=0 km?
+                  % PG: In the first 0 to 5km we will use velocity strenghtening 
+                  % And from 5-30km, velocity weakening. 
 p.N=p.NX*p.NW;
 p.DW(1:p.NW)=p.W/p.NW;
-p.DIP_W(1:p.NW)=17.0; % Extracted from the fault geometry (Luis)
+p.DIP_W(1:p.NW)=15.0; % Extracted from the fault geometry (Luis)
 twm=0.5;
 %ts=0.5;
 p.ACC = 1e-14;
 
-
 %% Defining space varying properties
-%year = 3600*24*365;
 %load('warmup_jp');
 p.OX_DYN = 1;
 p.OX_SEQ = 1;
@@ -90,18 +123,18 @@ p.OX_SEQ = 1;
 %p.N=p.NX*p.NW; % The manual only requires to define N for MESHDIM=1
 %p.L=400e3;
 %p.ACC=1e-10;
-
+sigma0 = p.SIGMA;
 p.A(1:p.NW) = p.A; % Along Dip
 p.B(1:p.NW) = p.B; % Along Dip
 p.SIGMA(1:p.NW) = p.SIGMA; % Along Dip
 p.DC(1:p.NW) = p.DC; % Along Dip
 p.Y = linspace(0,p.W,p.NW); % Along Dip
 p.Z = linspace(0,p.Z_CORNER,p.NW); % Along Dip
+% CHECK THIS
 p.V_0(1:p.NW) = p.V_SS; % Along Dip (Is this OK?). What is the difference between
                         % Reference steady state and the initial V0
                         % conditions?
 p.TH_0(1:p.NW) = p.DC/p.V_0; % (Along dip) From manual (TH_SS=DC/VS) Is TH_SS equal to TH_0 
-
 
 tmp_A=p.A;
 tmp_B=p.B;
@@ -111,9 +144,38 @@ tmp_Y=p.Y;
 tmp_Z=p.Z;
 tmp_DW=p.DW;
 tmp_DIP_W=p.DIP_W;
+tmp_CO(1:p.NW)=co;
 
 tmp_V_0=p.V_0;
 tmp_TH_0=p.TH_0;
+
+% Here changing SIGMA, A, B along depth.
+ba0=1.5;  %b/a at seismogenic zone
+bam=0.6;  %b/a at shallow/deeper part
+ZW = p.Z; 
+%% PG: Addapt the below parameters for Lima Fault has three transitions at depth. 
+% Finding layers:
+dd=10e3;    %depth of L sigma change
+idd=max(find(ZW+dd<=0));
+d1=2e3;     %upperbound of seismogenic zone (depth in m)
+id1=max(find(ZW+d1<=0));
+d2=5e3;     %limit of constant b/a
+id2=max(find(ZW+d2<=0));
+d3=12e3;    %lowerbound of seismogenic zone (depth in m)
+id3=max(find(ZW+d3<=0));
+d4=15e3;    %limit of constant b/a
+id4=max(find(ZW+d4<=0));
+ico_limit=max(find(ZW+co_limit<=0));
+
+tmp_B(1:id4)      = tmp_A(1:id4).*bam;
+tmp_B(id4+1:id3)  = tmp_A(id4+1:id3).*linspace(bam,ba0,numel(id4+1:id3));   %increasing a/b below seismogenic zone
+tmp_B(id3+1:id2)  = tmp_A(id3+1:id2).*ba0;   %a/b < 1 in seismogenic zone
+tmp_B(id2+1:id1)  = tmp_A(id2+1:id1).*linspace(ba0,bam,numel(id2+1:id1));
+tmp_B(id1+1:p.NW) = tmp_A(id1+1:p.NW).*bam;  %a/b >1 at shallow part 
+
+tmp_SIGMA(1:idd)      = sigma0;
+tmp_SIGMA(idd+1:p.NW) = linspace(sigma0,1e6,numel(idd+1:p.NW));
+tmp_CO(1:ico_limit)      = 0;
 
 % Replicate along the strike direction
 for i=1:p.NW
@@ -133,10 +195,11 @@ for i=1:p.NW
     p.DIP_W((i-1)*p.NX+1:i*p.NX) = tmp_DIP_W(i); 
 end
 
-
-
-
 % Lima fault potentially 6 asperities (2007,1974,1940,1966,1970,1996)
+% PG: 1970 Earhtquake is not subduction event. 1996 is too far away from Lima and
+% to save some computational time, we may need to obmit this event as well. In this way
+% we can use the exact dimension from Villegas fault p.L = 620Km. 
+
 n_asperities = 6;
 asp_center_x = [0.128 0.340 0.431 0.622 0.758 0.818]*p.L;
 asp_center_zy = [0.735 0.296 0.875 0.315 0.672 0.253]*p.W;
